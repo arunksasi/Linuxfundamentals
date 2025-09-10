70d9b67a0a1b86e8ee2e7478e87fa6010c3a0024
Arun K Sasi <arunsasi33@gmail.com>
2025-09-10 12:45:49 +0530

    Add new script for system monitoring

---
+++ monitor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+echo "==========CPU Load=============="
+
+uptime 
+
+echo ""
+echo  "========== Memory Usage =========="
+
+free -m 
+echo ""
+
+echo "========== Disk usage =========="
+
+df -h 
+
+echo ""
+echo "========== Network Usage =========="
+
+ip -s link 
+
+
+echo ""
+
+echo "========== Active users =========="
+
+who 
+
+
+echo "============ END ==================="
